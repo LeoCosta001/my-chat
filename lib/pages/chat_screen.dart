4d5f4a6b2fd9c0515bd8cd3880266dec30a927f3
LeoCosta <leocosta.black@hotmail.com>
2023-01-17 10:01:38 -0300

    Send text message to Firestore

--- lib/pages/chat_screen.dart
+++ lib/pages/chat_screen.dart
@@ -1,5 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_chat/widgets/text_composer.dart';
+
+enum FirebaseCoreStatus {
+  ok,
+  waiting,
+  error,
+}
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -9,6 +17,34 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  // Status
+  FirebaseCoreStatus _firebaseIsInitialized = FirebaseCoreStatus.waiting;
+
+  // Methods
+  // Initialize firebase core
+  Future<void> _initializeFirebaseCore() async {
+    Firebase.initializeApp().then((value) {
+      setState(() {
+        _firebaseIsInitialized = FirebaseCoreStatus.ok;
+      });
+    }).catchError((onError) {
+      setState(() {
+        _firebaseIsInitialized = FirebaseCoreStatus.error;
+      });
+    });
+  }
+
+  // Send a new message to Firestore
+  Future<void> _sendMessage(String message) async {
+    FirebaseFirestore.instance.collection('messages').doc().set({'text': message});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeFirebaseCore();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +54,7 @@ class _ChatScreenState extends State<ChatScreen> {
       ),
       body: Container(
         margin: const EdgeInsets.symmetric(horizontal: 12),
-        child: const TextComposer(),
+        child: TextComposer(_sendMessage),
       ),
     );
   }
--- lib/widgets/text_composer.dart
+++ lib/widgets/text_composer.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class TextComposer extends StatefulWidget {
-  const TextComposer({super.key});
+  const TextComposer(this.sendMessage, {super.key});
+
+  final Function(String message) sendMessage;
 
   @override
   State<TextComposer> createState() => _TextComposerState();
@@ -11,29 +13,42 @@ class _TextComposerState extends State<TextComposer> {
   // States
   bool _isEmpty = true;
 
+  // Controllers
+  final TextEditingController textComposerController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         IconButton(
-          onPressed: (() {}),
+          onPressed: () => {},
           icon: const Icon(Icons.photo_camera),
         ),
         Expanded(
           child: TextField(
+            controller: textComposerController,
             // ".collapsed" Remove o espaço extra em baixo do input de texto
             decoration: const InputDecoration.collapsed(hintText: 'Send a message'),
             onChanged: (value) => setState(() {
               _isEmpty = value.isEmpty;
             }),
-            onSubmitted: (value) {},
+            onSubmitted: (value) => sendMessage(value),
           ),
         ),
         IconButton(
-          onPressed: _isEmpty ? null : () {},
+          onPressed: _isEmpty ? null : () => sendMessage(textComposerController.text),
           icon: const Icon(Icons.send),
         ),
       ],
     );
+  }
+
+  // Methods
+  void sendMessage(String message) {
+    widget.sendMessage(message);
+    textComposerController.clear();
+    setState(() {
+      _isEmpty = true;
+    });
   }
 }
