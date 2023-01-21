8dae7c1f80dcb266fc4bb8fcc06f9758ae9d6bec
LeoCosta <leocosta.black@hotmail.com>
2023-01-20 23:13:33 -0300

    Create authentication by google account

--- lib/pages/chat_screen.dart
+++ lib/pages/chat_screen.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:my_chat/widgets/text_composer.dart';
 
@@ -21,6 +23,13 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  // Cria uma instancia de logion com a conta do google
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final GlobalKey<ScaffoldMessengerState> _scaffoldKey = GlobalKey<ScaffoldMessengerState>();
+
+  // States
+  User? currentUser;
+
   // Status
   FirebaseCoreStatus _firebaseIsInitialized = FirebaseCoreStatus.waiting;
 
@@ -31,6 +40,7 @@ class _ChatScreenState extends State<ChatScreen> {
       setState(() {
         _firebaseIsInitialized = FirebaseCoreStatus.ok;
       });
+      _checkUserAuthentication();
     }).catchError((onError) {
       setState(() {
         _firebaseIsInitialized = FirebaseCoreStatus.error;
@@ -38,17 +48,79 @@ class _ChatScreenState extends State<ChatScreen> {
     });
   }
 
+  // Atualiza automaticamente a autenticação do usuário
+  void _checkUserAuthentication() {
+    // Sempre que tiver alguma alteração relacionada a autenticação atual do usuário no firebase esta função de callback será executada
+    FirebaseAuth.instance.userChanges().listen((User? user) {
+      setState(() {
+        currentUser = user;
+      });
+    });
+  }
+
+  // Obtem o acesso de um usuário a partir da conta google
+  // OBS: Fazendo uma função/fluxo dessa forma faz com que sempre que o usuário estiver expirado sejá obtido um novo acesso
+  Future<User?> _getUser() async {
+    if (currentUser != null) return currentUser;
+
+    try {
+      // Obtem as informações de usuário de alguém que fez login com o google
+      final GoogleSignInAccount? googleSignInAccount = await googleSignIn.signIn();
+
+      // TODO: Create validation
+      if (googleSignInAccount != null) {
+        // Extrair os tokens de autenticação de login
+        final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+
+        // Cria uma credencial para autenticação de login no Firebase Authentication
+        // OBS: Para fazer login com outras contas (facebook, twitter, etc) tambem será usado um método para criar credencial (nos outros casos não será o "GoogleAuthProvider")
+        final AuthCredential credential = GoogleAuthProvider.credential(
+          idToken: googleSignInAuthentication.idToken,
+          accessToken: googleSignInAuthentication.accessToken,
+        );
+
+        // Faz login no Firebase Authentication com ama credencial
+        // OBS: Para fazer login com outras contas (facebook, twitter, etc) usa credenciais desta mesma forma)
+        final UserCredential authResult = await FirebaseAuth.instance.signInWithCredential(credential);
+
+        // Obtem os dados do usuário logado
+        final User? user = authResult.user;
+
+        return user;
+      }
+    } catch (error) {
+      // print(error);
+      return null;
+    }
+  }
+
   // Send a new message to Firestore
-  Future<void> _createNewMessage({String message = '', bool? isImage}) async {
-    FirebaseFirestore.instance.collection('messages').doc().set({isImage == true ? 'imageUrl' : 'text': message});
+  Future<void> _createNewMessage(Map<String, dynamic> messagePayload) async {
+    FirebaseFirestore.instance.collection('messages').doc().set({
+      ...messagePayload,
+      'createAt': DateTime.now().millisecondsSinceEpoch,
+    });
   }
 
   // Send a new message or a image (to Firestore and Storage)
   Future<void> _sendMessage({String? message, XFile? imageFile}) async {
-    Map<String, dynamic> messagePayload = {};
+    final User? user = await _getUser();
+    if (user == null) {
+      // Avisa o usuário sobre a falha de autenticação
+      _showLoginErrorMessage();
+    }
+
+    Map<String, dynamic> messagePayload = {
+      'uid': user?.uid,
+      'userName': user?.displayName,
+      'userPhoto': user?.photoURL,
+      'text': null,
+      'imageUrl': null
+    };
+
     if (message != null) {
-      _createNewMessage(message: message);
       messagePayload['text'] = message;
+      _createNewMessage(messagePayload);
     }
 
     // Send new image to Firebase Storage
@@ -77,11 +149,26 @@ class _ChatScreenState extends State<ChatScreen> {
             // Handle successful uploads on complete
             // ...
             taskSnapshot.ref.getDownloadURL().then((value) {
-              _createNewMessage(message: value, isImage: true);
+              messagePayload['imageUrl'] = value;
+              _createNewMessage(messagePayload);
             });
             break;
         }
       });
+    }
+  }
+
+  // Exibe um Snackbar com a mensagem de erro no login
+  void _showLoginErrorMessage() {
+    final ScaffoldMessengerState? scaffoldKeyCurrentState = _scaffoldKey.currentState;
+    if (scaffoldKeyCurrentState != null) {
+      // OBS: Esta é a forma de exibir o snackbar sem o "BuildContext"
+      scaffoldKeyCurrentState.showSnackBar(
+        const SnackBar(
+          content: Text('Authentication error'),
+          backgroundColor: Colors.red,
+        ),
+      );
     }
   }
 
@@ -94,13 +181,58 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: const Text('My Chat'),
         elevation: 0,
       ),
       body: Container(
         margin: const EdgeInsets.symmetric(horizontal: 12),
-        child: TextComposer(_sendMessage),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              // O widget "StreamBuilder" vai executar novamente o atributo "builder" sempre que houver alteração no valor do atributo "stream"
+              // Neste caso ele será atualizado sempre que houver alteração no documento "message" do banco de dados
+              child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+                stream: _firebaseIsInitialized == FirebaseCoreStatus.ok ? FirebaseFirestore.instance.collection('messages').snapshots() : null,
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.none:
+                    case ConnectionState.waiting:
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    default:
+                      if (snapshot.data != null) {
+                        List<QueryDocumentSnapshot<Map<String, dynamic>>> messageList = snapshot.data!.docs;
+                        messageList.sort((a, b) => b['createAt'].compareTo(a['createAt']));
+
+                        // A renderização da lista é realizada com o widget "ListView.builder" para que seja rrenderizado em tela apenas o que está sendo visto atualmente pelo usuário
+                        return ListView.builder(
+                          itemCount: messageList.length,
+                          // Faz a lista começar de baixo para cima (no caso as mensagens)
+                          reverse: true,
+                          itemBuilder: ((context, index) {
+                            late String? messageText = messageList[index].data()['text'];
+                            late String? messageImage = messageList[index].data()['imageUrl'];
+                            return ListTile(
+                              title: Text('${messageText ?? messageImage}'),
+                            );
+                          }),
+                        );
+                      } else {
+                        return const Center(
+                          child: Text('Unknown error... :('),
+                        );
+                      }
+                  }
+                },
+              ),
+            ),
+            TextComposer(_sendMessage),
+          ],
+        ),
       ),
     );
   }
